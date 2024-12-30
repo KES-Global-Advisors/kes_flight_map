a0657b2ee576acfc7fb00f5caed8c9b28bb01e6d
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2024-12-30 11:25:05 +0200

    create Program model

--- flight_map/models.py
+++ flight_map/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Program(models.Model):
+    """Create a program"""
+    name = models.CharField(max_length=255)
+    vision = models.TextField()
+    time_horizon = models.CharField(max_length=100)
+    stakeholders = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="programs")
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Program"
+        verbose_name_plural = "Programs"
+        ordering = ["name"]
