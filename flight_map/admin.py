c5eb57aabeb272824a7990ba941a49d68fea8558
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2025-01-22 13:20:40 +0200

    register new models

--- flight_map/admin.py
+++ flight_map/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Program, Workstream, Milestone, Task
+from .models import Strategy,StrategicGoal, Program, Workstream, Milestone, Activity
 
 # Register your models here.
+admin.site.register(Strategy)
+admin.site.register(StrategicGoal)
 admin.site.register(Program)
 admin.site.register(Workstream)
 admin.site.register(Milestone)
-admin.site.register(Task)
+admin.site.register(Activity)
