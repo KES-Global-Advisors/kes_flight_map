7dd5b53f90c6dbf410d98d35dccf978f622010d7
Kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2025-01-22 13:13:12 +0200

    update serializers

--- flight_map/serializers.py
+++ flight_map/serializers.py
@@ -1,93 +1,58 @@
 from rest_framework import serializers
-from .models import Program, Workstream, Milestone, Task
+from .models import Strategy, StrategicGoal, Program, Workstream, Milestone, Activity
 from django.contrib.auth import get_user_model
 
 # Dynamically retrieve the user model based on AUTH_USER_MODEL setting
 User = get_user_model()
+class StrategySerializer(serializers.ModelSerializer):
+    executive_sponsors = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    strategy_leads = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    communication_leads = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+
+    class Meta:
+        model = Strategy
+        fields = '__all__'
+
+
+class StrategicGoalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = StrategicGoal
+        fields = '__all__'
+
+
 class ProgramSerializer(serializers.ModelSerializer):
-    """
-    This field represents the many-to-many relationship between Program and User.
-    StringRelatedField returns the string representation of the related objects (usernames in this case).
-    """
-    stakeholders = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    executive_sponsors = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    program_leads = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    workforce_sponsors = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    key_improvement_targets = serializers.PrimaryKeyRelatedField(many=True, queryset=StrategicGoal.objects.filter(category="business"))
+    key_organizational_goals = serializers.PrimaryKeyRelatedField(many=True, queryset=StrategicGoal.objects.filter(category="organizational"))
 
     class Meta:
         model = Program
         fields = '__all__'
 
 
-    def to_representation(self, instance):
-        """
-        Overrides the representation to include detailed stakeholder information.
-        """
-        representation = super().to_representation(instance)
-        representation['stakeholders'] = [
-            {"id": user.id, "username": user.username} for user in instance.stakeholders.all()
-        ]
-        return representation
-
 class WorkstreamSerializer(serializers.ModelSerializer):
-    """
-    Handles program as a writable field for creation while maintaining a nested
-    representation for read operations.
-    """
-    program = serializers.PrimaryKeyRelatedField(queryset=Program.objects.all())
+    workstream_leads = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
+    team_members = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
 
     class Meta:
         model = Workstream
         fields = '__all__'
 
 
-    def to_representation(self, instance):
-        """
-        Overrides the representation to include detailed program information.
-        """
-        representation = super().to_representation(instance)
-        representation['program'] = ProgramSerializer(instance.program).data
-        return representation
-
 class MilestoneSerializer(serializers.ModelSerializer):
-    """
-    Handles the many-to-many relationship between Milestone objects.
-    Includes primary keys for dependencies during creation and nested data during read operations.
-    """
-    dependencies = serializers.PrimaryKeyRelatedField(
-        many=True,
-        queryset=Milestone.objects.all()
-    )
-
     class Meta:
         model = Milestone
         fields = '__all__'
 
-    def to_representation(self, instance):
-        """
-        Overrides the representation to include nested milestone dependency details.
-        """
-        representation = super().to_representation(instance)
-        representation['dependencies'] = MilestoneSerializer(instance.dependencies.all(), many=True).data
-        return representation
 
+class ActivitySerializer(serializers.ModelSerializer):
+    prerequisite_activities = serializers.PrimaryKeyRelatedField(many=True, queryset=Activity.objects.all())
+    parallel_activities = serializers.PrimaryKeyRelatedField(many=True, queryset=Activity.objects.all())
+    successive_activities = serializers.PrimaryKeyRelatedField(many=True, queryset=Activity.objects.all())
 
-class TaskSerializer(serializers.ModelSerializer):
-    """
-    Handles milestone as a writable field for creation while maintaining a nested
-    representation for read operations.
-    """
-    milestone = serializers.PrimaryKeyRelatedField(queryset=Milestone.objects.all())
-    key_stakeholders = serializers.PrimaryKeyRelatedField(many=True, queryset=User.objects.all())
     class Meta:
-        model = Task
+        model = Activity
         fields = '__all__'
 
-
-    def to_representation(self, instance):
-        """
-        Overrides the representation to include detailed milestone information.
-        """
-        representation = super().to_representation(instance)
-        representation['milestone'] = MilestoneSerializer(instance.milestone).data
-        representation['key_stakeholders'] = [
-            {"id": user.id, "username": user.username} for user in instance.key_stakeholders.all()
-        ]
-        return representation
